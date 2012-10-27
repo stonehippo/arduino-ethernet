e8e675d47c3e340b9f547fb3e8fb2e9e9413f115
George White <stonehippo@gmail.com>
2012-10-27 01:57:08 -0400

    Changed the copyright statement on the utility files

--- utility/EthernetCompat.cpp
+++ utility/EthernetCompat.cpp
@@ -1,20 +1,20 @@
 //  Copyright (C) 2010 Georg Kaindl
 //  http://gkaindl.com
 //
-//  This file is part of Arduino EthernetBonjour.
+//  This file is part of Arduino Ethernet.
 //
-//  EthernetBonjour is free software: you can redistribute it and/or
+//  Arduino Ethernet is free software: you can redistribute it and/or
 //  modify it under the terms of the GNU Lesser General Public License
 //  as published by the Free Software Foundation, either version 3 of
 //  the License, or (at your option) any later version.
 //
-//  EthernetBonjour is distributed in the hope that it will be useful,
+//  Arduino Ethernet is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU Lesser General Public License for more details.
 //
 //  You should have received a copy of the GNU Lesser General Public
-//  License along with EthernetBonjour. If not, see
+//  License along with Arduino Ethernet. If not, see
 //  <http://www.gnu.org/licenses/>.
 //
 
--- utility/EthernetUtil.h
+++ utility/EthernetUtil.h
@@ -1,9 +1,9 @@
 //  Copyright (C) 2010 Georg Kaindl
 //  http://gkaindl.com
 //
-//  This file is part of Arduino EthernetBonjour.
+//  This file is part of Arduino Ethernet.
 //
-//  EthernetBonjour is free software: you can redistribute it and/or
+//  Arduino Ethernet is free software: you can redistribute it and/or
 //  modify it under the terms of the GNU Lesser General Public License
 //  as published by the Free Software Foundation, either version 3 of
 //  the License, or (at your option) any later version.
@@ -14,7 +14,7 @@
 //  GNU Lesser General Public License for more details.
 //
 //  You should have received a copy of the GNU Lesser General Public
-//  License along with EthernetBonjour. If not, see
+//  License along with Arduino Ethernet. If not, see
 //  <http://www.gnu.org/licenses/>.
 //
 
